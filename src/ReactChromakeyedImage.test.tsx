83dafbd8619b3a25c1f9d6c7cba058f8dc1a3c35
themillhousegroup <john.marshall.au@gmail.com>
2025-02-06 14:45:48 +1100

    Update test with actual online image

--- src/ReactChromakeyedImage.test.tsx
+++ src/ReactChromakeyedImage.test.tsx
@@ -3,8 +3,10 @@ import * as React from 'react'
 
 import ReactChromakeyedImage from '../reactChromakeyedImage'
 
+const IMG_SRC = "https://placecats.com/bella/300/200";
+
 test('renders correctly on first load', () => {
-	render(<ReactChromakeyedImage />)
+	render(<ReactChromakeyedImage src={IMG_SRC} findColor="#f0f0f0" replaceColor="#00f000" tolerance={50} />)
 	const button = screen.getByRole('button')
 	expect(button).toHaveTextContent('0')
 })
