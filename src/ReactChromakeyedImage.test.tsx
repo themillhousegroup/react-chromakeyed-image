6912e8046591f8a21795e38a8db2a6f5ef7c1ccf
themillhousegroup <john.marshall.au@gmail.com>
2025-02-06 15:28:06 +1100

    Update ReactChromakeyedImage.test.tsx

--- src/ReactChromakeyedImage.test.tsx
+++ src/ReactChromakeyedImage.test.tsx
@@ -5,8 +5,15 @@ import ReactChromakeyedImage from '../reactChromakeyedImage'
 
 const IMG_SRC = "https://placecats.com/bella/300/200";
 
-test('renders correctly on first load', () => {
-	render(<ReactChromakeyedImage src={IMG_SRC} findColor="#f0f0f0" replaceColor="#00f000" tolerance={50} />)
-	const button = screen.getByRole('button')
-	expect(button).toHaveTextContent('0')
+test('renders correctly on first load', async () => {
+	const TEST_ID = "rci-test-id-1"
+	render(<ReactChromakeyedImage 
+		       src={IMG_SRC} 
+		       findColor="#f0f0f0" 
+		       replaceColor="#00f000" 
+		       tolerance={50} 
+		       data-test-id={TEST_ID}
+       	/>)
+	const canvas = await screen.findByTestId(TEST_ID)
+	expect(canvas).toBeInTheDocument()
 })
